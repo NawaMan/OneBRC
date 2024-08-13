4e5a631f23e547c6ebc426b3b5fcbd62f433ad95
Nawa Manusitthipol <nawa@nawaman.net>
2024-08-13 17:46:42 -0400

    Expand more -- sort of working.

--- docs-src/src/routes/+layout.svelte
+++ docs-src/src/routes/+layout.svelte
@@ -9,11 +9,11 @@
 	import Copyright from '$lib/components/Copyright.svelte';
 
 	let container: any;
+	let content: any;
 
 	let isScaled = false;
 
 	if (browser) {
-		// Subscribe to the store to get the current scale mode value
 		scaleMode.subscribe(value => {
 			isScaled = value;
 
@@ -25,30 +25,37 @@
 				if (container) {
 					container.style.width  = '1280px';
 					container.style.height =  '720px';
+					content.style.transform = `scale(1)`;
+					content.style.transformOrigin = 'top left';
 				}
 			}
 		});
 	}
 
 	function adjustSize() {
-		if (!container) return;
+		if (!container)
+			return;
+
 		const aspectRatio = 1280 / 720;
 		const windowWidth = window.innerWidth;
 		const windowHeight = window.innerHeight;
 		const windowRatio = windowWidth / windowHeight;
 
 		if (windowRatio > aspectRatio) {
-			container.style.height = '100vh';
-			container.style.width = `${100 * aspectRatio}vh`;
+			container.style.height = 'calc(100vh - 10px)';
+			container.style.width = `${Math.round(container.offsetHeight*aspectRatio)}px`;
 		} else {
-			container.style.width = '100vw';
-			container.style.height = `${100 / aspectRatio}vw`;
+			container.style.width = 'calc(100vw - 10px)';
+			container.style.height = `${Math.round(container.offsetWidth / aspectRatio)}px`;
 		}
+		let scale = (container.offsetWidth - 10) / 1280.0;
+		content.style.transform = `scale(${scale})`;
+		content.style.transformOrigin = 'top left';
 	}
 </script>
 
 <div class="container" class:scale-mode={isScaled} bind:this={container}>
-	<div class="content">
+	<div class="content"  bind:this={content}>
 		<TableOfContent />
 		<SizeMode />
 		<slot />
@@ -62,6 +69,7 @@
 		height: 720px;
 		margin: 0 auto;
 		display: flex;
+		background: #112c63;
 		border: 5px solid #ccc; /* Optional: adds a border to visualize the container */
 		position: relative;
 	}
@@ -72,12 +80,14 @@
 		max-height: 100vh;
 	}
 	.content {
-		width: 100%;
-		height: calc(100% - 20px);
-		overflow: auto;
+		width: calc(1280px - 20px);
+		height: calc(720px - 20px);
+		min-width: calc(1280px - 20px);
+		min-height: calc(720px - 20px);
+		overflow: hidden;
 		padding: 10px;
-		margin: 0 auto;
-		display: flex;
+		margin: 0px;
+		display: block;
 		justify-content: center;
 		align-items: center;
 		color: #C0F1FF;
---
+++ docs/_app/immutable/entry/start.CnLI7asp.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.9V3IdnA8.js";export{t as start};
