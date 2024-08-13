4ba3fa29dc44224a0637111be1c788cc02e1c24d
Nawa Manusitthipol <nawa@nawaman.net>
2024-08-13 00:50:26 -0400

    Small adjustment to +page.svelte content.

--- docs-src/src/routes/introduction.html/+page.svelte
+++ docs-src/src/routes/introduction.html/+page.svelte
@@ -68,7 +68,7 @@
 			</ul>
 		</li>
 		<li>Calculate min, max and average temperatures <br/>for each station.</li>
-		<li>Print the results</li>
+		<li>Print the results in Java sorted map `toString()`</li>
 		<li>No external dependency</li>
 		<li>Source code in one file</li>
 		<li>Benchmark on 32 cores CPU and 128 GB RAM</li>
@@ -77,9 +77,9 @@
 	<div class="right">
 		<table>
 			<tr><td>1</td><td>Ottawa;14.2</td></tr>
-			<tr><td>2</td><td>Bangkok;35.0</td></tr>
+			<tr><td>2</td><td>Bangkok;44.0</td></tr>
 			<tr><td>3</td><td>Sydney;-2.5</td></tr>
-			<tr><td>4</td><td>Abéché;89.3</td></tr>
+			<tr><td>4</td><td>Łódź;-20.4</td></tr>
 			<tr class="blur"><td>455</td><td>New York;20.7</td></tr>
 			<tr class="blur"><td>15,466</td><td>Mars;-24.7</td></tr>
 			<tr class="blur"><td>1,546,536</td><td>Venus;84.5</td></tr>
