30bd670207176cd0bf2f287c379eb6df0a013abb
Nawa Manusitthipol <nawa@nawaman.net>
2024-09-09 00:55:16 -0400

    Move docs.

--- docs-src/docs/_app/immutable/chunks/scaleMode.CD7-bn0n.js
+++
@@ -1,1 +0,0 @@
-import{w as o}from"./index.sg1ewLX7.js";let t=!1;{const e=localStorage.getItem("scaleMode");e!==null&&(t=e==="true")}const l=o(t);l.subscribe(e=>{localStorage.setItem("scaleMode",e.toString())});export{l as s};
--- docs/_app/immutable/entry/start.CFdDH5qU.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DzwNGpNc.js";export{t as start};
