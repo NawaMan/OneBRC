f38c7075d4e769d3e4eacbe3542e77d960b7d686
Nawa Manusitthipol <nawa@nawaman.net>
2024-09-15 18:45:25 -0400

    text

---
+++ docs-src/src/routes/text/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import '$lib/styles/global.css';
+	import '$lib/styles/tooltip.css';
+</script>
+
+<slot />
---
+++ docs-src/src/routes/text/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		document.body.classList.remove('rendering');
+	});
+</script>
+
+<div id="background">
+	<div id="main">
+		<div>
+			<div>We have not got a change to write an article for this yet.</div>
+			<div>But we do have <a href="../slides/onebrc.html">presentation slides</a> you might want to check it out.</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	#background {
+		background-color: #f0ead2;
+		padding: 0px;
+		margin: 0px;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+	}
+
+	#main {
+		color: black;
+		background-color: #fff;
+		width: calc(100vw*0.8);
+		height: calc(100vh*0.8);
+		margin-top: calc(100vh*0.1);
+		margin-left: auto;
+		margin-right: auto;
+		border-radius: 10px;
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+		display: flex;
+
+		font-size: x-large;
+	}
+
+	a::before,
+	a::after {
+		content: " ";
+	}
+</style>
