f5aa2b93d34bd7ec8a940bb3186b6d5fab3703e8
Nawa Manusitthipol <nawa@nawaman.net>
2024-09-23 00:06:32 -0400

    Allow to specify the Box position.

--- docs-src/src/lib/components/Box.svelte
+++ docs-src/src/lib/components/Box.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-	export let expanded = false; // Control expansion
-	export let width; // Image width
-	export let height; // Image height
-	export let linkUrl: string | null = null; // Link
-	export let linkText = 'LINK'; // Link
-	export let onClick: (() => void) | null = null;
+	export let width;
+	export let height;
+
+	export let expanded : boolean       = false;
+	export let showClose: boolean       = true;
+	export let top      : string | null = null;
+	export let left     : string | null = null;
+	export let bottom   : string | null = null;
+	export let right    : string | null = null;
+	export let linkUrl  : string | null = null;
+	export let linkText : string        = 'LINK';
+
+	export let shadowOpacity: number              = 0.5;
+	export let onClick      : (() => void) | null = null;
 
 	function collapse() {
 		expanded = false;
 	}
 </script>
 
-<div class="parent {expanded ? 'expanded' : ''}">
+<div
+	class="parent {expanded ? 'expanded' : ''}"
+	style="--custom-top: {top}; --custom-left: {left}; --custom-bottom: {bottom}; --custom-right: {right}; --shadow-opacity: {shadowOpacity};"
+>
 	<div
 		class="img-box"
 		style="--full-width: {width}px; --full-height: {height}px;"
@@ -20,9 +31,11 @@
 		role="button"
 		tabindex="-1">
 		<slot />
+		{#if showClose}
 		<div class="close" on:click={collapse} on:keydown={collapse} role="button" tabindex="-1">
 			CLOSE
 		</div>
+		{/if}
 
 		{#if linkUrl}
 			<div class="link"><a href={linkUrl} target="_blank">{linkText}</a></div>
@@ -37,6 +50,11 @@
 		position: absolute;
 		width: 0px;
 		height: 0px;
+		top: var(--custom-top, 50%);
+		left: var(--custom-left, 50%);
+		bottom: var(--custom-bottom, auto);
+		right: var(--custom-right, auto);
+		transform: translate(-50%, -50%);
 	}
 
     .parent .link a {
@@ -138,7 +156,7 @@
 		margin-left: calc(50% - var(--full-width) / 2);
 		box-shadow:
 			2px 2px 10px black,
-			0 0 0 9999px rgb(0, 0, 0, 0.5);
+			0 0 0 9999px rgb(0, 0, 0, var(--shadow-opacity));
 		transition:
 			var(--box-ani-time) calc(0 * var(--box-ani-time)) width,
 			var(--box-ani-time) calc(1 * var(--box-ani-time)) height,
